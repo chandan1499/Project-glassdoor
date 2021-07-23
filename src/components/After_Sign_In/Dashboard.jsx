169f7f9072e75425032182ccf5b87df330aca6f3
Chandan gupta <chandan1499>
2021-07-23 14:44:40 +0530

    compare companies

--- src/components/After_Sign_In/Dashboard.jsx
+++ src/components/After_Sign_In/Dashboard.jsx
@@ -4,14 +4,14 @@ import SignIn3mix from './New_mix';
 import SuggestMix from './Suggest_mix';
 import RecentMix from './Recent_mix';
 import styles from './After_sign_in_2.module.css'
-// import { Navbar } from '../navbar';
+import { Navbar } from '../navbar';
 import { Footer } from '../footer';
 
 const Dashboard = () => {
 
 return (
     <>
-    {/* <Navbar /> */}
+    <Navbar />
     <div>
     <div className={styles.after_sign_in_upper_whole}>
     <div className={styles.after_sign_in_heading}>
